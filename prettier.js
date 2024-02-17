f3f20e24cb26e4186b64711f79fe14c57ca6c585
woodmost <santiago.woodmost@outlook.com>
2024-02-17 18:13:39 +0800

    chore: add code formatting
    add next.js style guide: prettier, eslint, husky, lint-staged and commitizen

---
+++ prettier.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+};
